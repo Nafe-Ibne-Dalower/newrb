21f816efb28677fd6646c3794a5105dad5f1b399
Nafe-Ibne-Dalower <121440579+Nafe-Ibne-Dalower@users.noreply.github.com>
2023-02-20 20:08:18 +0600

    Update index.js

--- index.js
+++ index.js
@@ -47,4 +47,4 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
 // Export the Express API
-module.exports = app;
+// module.exports = app;
